15953dd15ddc327f3caadf486a79c6a5dddf1e5f
Amirhossein Roudgar <2000Amir1379@gmail.com>
2023-03-30 16:54:00 +0330

    changed auction photo remove url

--- saku/auction/urls.py
+++ saku/auction/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("categories/", CategoryList.as_view()),
     path("<str:token>", DetailedAuction.as_view(), name="detailed_auction"),
     path(
-        "picture/<str:token>",
+        "remove-picture/<str:token>",
         DeleteAuctionPicture.as_view(),
         name="delete_auction_picture",
     ),
