4a98837c518175716b51163cb31f98192d61f8ec
Amirhossein Roudgar <2000Amir1379@gmail.com>
2023-04-05 20:41:37 +0330

    fixed .env import bug in settings.py using dotenv_values() from python-dotenv package

--- saku/saku/settings.py
+++ saku/saku/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import dotenv_values
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,6 +25,9 @@ DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
 ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
+
+# Initialize Environment variables
+env_config = dotenv_values("../.env")
 
 # Application definition
 
@@ -81,25 +85,14 @@ WSGI_APPLICATION = "saku.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "saku-db",
-        "USER": "postgres",
-        "PASSWORD": "saku1234",
-        "HOST": os.getenv("DB_HOST", "db"),
-        "PORT": "5432",
-        # "NAME": os.getenv("DB_NAME", "saku-db"),
-        # "USER": os.getenv("DB_USER", "postgres"),
-        # "PASSWORD": os.getenv("DB_PASSWORD", "saku1234"),
-        # "HOST": os.getenv("DB_HOST", "db"),
-        # "PORT": os.getenv("DB_PORT", "5432"),
-        # "NAME": env('DB_NAME'),
-        # "USER": env('DB_USER'),
-        # "PASSWORD": env('DB_PASSWORD'),
-        # "HOST": env('DB_HOST'),
-        # "PORT": env('DB_PORT'),
+        "NAME": env_config['DB_NAME'],
+        "USER": env_config['DB_USER'],
+        "PASSWORD": env_config['DB_PASSWORD'],
+        "HOST": env_config['DB_HOST'],
+        "PORT": env_config['DB_PORT'],
     },
     "local": {"ENGINE": "django.db.backends.sqlite3", "NAME": "./saku.db"},
 }
