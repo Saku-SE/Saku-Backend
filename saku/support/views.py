b0e2aaadb8df9aca81e6339abeeb97b2188941af
Amirhossein Roudgar <2000Amir1379@gmail.com>
2023-04-19 10:11:20 +0330

    feat: added functionality for advice api

--- saku/support/views.py
+++ saku/support/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+# from django.shortcuts import 
+
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from support.maps import support_q_maps
 
 # Create your views here.
+
+
+class GeneralAdviceView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            answer_values = [support_q_maps[str(request.data[key])] for key in support_q_maps.keys()]
+            final_value = sum(answer_values)
+
+            # result = 0
+            if final_value < 0:
+                result = 0
+            elif final_value <= len(support_q_maps) / 2:
+                result = 1
+            elif final_value  < len(support_q_maps):
+                result = 2
+            else:
+                result = 3
+            
+            response = {
+                "status": "success",
+                "code": status.HTTP_200_OK,
+                "data": {
+                    "level": result
+                }
+            }
+
+        except:
+            
+            response = {
+                "status": "error",
+                "code": status.HTTP_400_BAD_REQUEST,
+                "message": "There is a problem with the data you have sent.",
+                "data": []
+            }
+
+        return Response(response, status=response["code"])
+
