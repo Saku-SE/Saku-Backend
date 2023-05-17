7373d37deb636a135a10608ce31fad35e29cffe6
Amirhossein Roudgar <2000Amir1379@gmail.com>
2023-05-17 16:10:48 +0330

    feat: added url for subscription purchase API view

--- saku/subscription/urls.py
+++ saku/subscription/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SubscriptionListView
+from .views import SubscriptionListView, PurchaseSubscriptionView
 
 app_name = "subscription"
 urlpatterns = [
     path("list", SubscriptionListView.as_view(), name="subscription-list"),
+    path("purchase", PurchaseSubscriptionView.as_view(), name="purchase-subscription"),
 ]
